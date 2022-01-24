ecfb6a0e479a53a37537abd82ad5232b8926ee9e
Junior Paula <slucivaldo509@gmail.com>
2022-01-23 23:03:42 -0300

    implements logic from the aplication

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useQuery } from 'react-query';
+import Item from './Item';
+import Cart from './Cart';
 
 import { Drawer } from '@material-ui/core';
 import { LinearProgress } from '@material-ui/core';
@@ -7,7 +9,7 @@ import { Grid } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
 import { Badge } from '@material-ui/core';
 
-import { Wrapper } from './App.styles';
+import { StyledButton, Title, Wrapper } from './App.styles';
 import { ICartItemType } from './interfaces/ICartItemType';
 import { BASE_URL } from './config/urlConfig';
 
@@ -15,19 +17,73 @@ const getProducts = async (): Promise<ICartItemType[]> => {
   return await (await fetch(BASE_URL)).json();
 };
 
-const App = () => {
+const App: React.FC = () => {
+  const [cartOpen, setCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as ICartItemType[]);
+
   const { data, isLoading, error } = useQuery<ICartItemType[]>(
     'products',
     getProducts,
   );
-  console.log(data);
 
-  const getTotalItem = () => null;
-  const handleAddToCart = () => null;
-  const handleRemoveFromCart = () => null;
+  const getTotalItems = (items: ICartItemType[]) => {
+    return items.reduce((ack: number, item) => ack + item.amount, 0);
+  };
+  const handleAddToCart = (clickedItem: ICartItemType) => {
+    setCartItems((prev) => {
+      const isItemInCart = prev.find((item) => item.id === clickedItem.id);
 
-  // eslint-disable-next-line react/react-in-jsx-scope
-  return <div className="App">start</div>;
+      if (isItemInCart) {
+        return prev.map((item) =>
+          item.id === clickedItem.id
+            ? { ...item, amount: item.amount + 1 }
+            : item,
+        );
+      }
+
+      return [...prev, { ...clickedItem, amount: 1 }];
+    });
+  };
+  const handleRemoveFromCart = (id: number) => {
+    setCartItems((prev) =>
+      prev.reduce((ack, item) => {
+        if (item.id === id) {
+          if (item.amount === 1) return ack;
+          return [...ack, { ...item, amount: item.amount - 1 }];
+        } else {
+          return [...ack, item];
+        }
+      }, [] as ICartItemType[]),
+    );
+  };
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Ops!! Algo deu errado. ;(</div>;
+
+  return (
+    <Wrapper>
+      <Title>HortiFruti</Title>
+      <Drawer anchor="right" open={cartOpen} onClose={() => setCartOpen(false)}>
+        <Cart
+          cartItem={cartItems}
+          addToCart={handleAddToCart}
+          removeFromCart={handleRemoveFromCart}
+        />
+      </Drawer>
+      <StyledButton onClick={() => setCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color="error">
+          <AddShoppingCart />
+        </Badge>
+      </StyledButton>
+      <Grid container spacing={3}>
+        {data?.map((item) => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
+  );
 };
 
 export default App;
